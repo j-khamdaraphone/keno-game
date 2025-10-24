0f294cd69a8a7358283cf6672cf5357f313ec4c8
Jennifer Khamdaraphone <jkhamdaraphone@gmail.com>
2025-10-24 15:29:11 -0500

    Menu visible

--- JavaFX_MavenTemplate_VS1/src/main/java/JavaFXTemplate.java
+++ JavaFX_MavenTemplate_VS1/src/main/java/JavaFXTemplate.java
@@ -24,34 +24,13 @@ public class JavaFXTemplate extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		 Rectangle rect = new Rectangle (100, 40, 100, 100);
-	     rect.setArcHeight(50);
-	     rect.setArcWidth(50);
-	     rect.setFill(Color.VIOLET);
+         primaryStage.setTitle("Keno");
 
-	     RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
-	     rt.setByAngle(270);
-	     rt.setCycleCount(4);
-	     rt.setAutoReverse(true);
-	     SequentialTransition seqTransition = new SequentialTransition (
-	         new PauseTransition(Duration.millis(500)),
-	         rt
-	     );
-	     seqTransition.play();
-	     
-	     FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
-	     ft.setFromValue(1.0);
-	     ft.setToValue(0.3);
-	     ft.setCycleCount(4);
-	     ft.setAutoReverse(true);
-
-	     ft.play();
 	     BorderPane root = new BorderPane();
-	     root.setCenter(rect);
-	     
-	     Scene scene = new Scene(root, 700,700);
+	     KenoMenu kenoMenu = new KenoMenu();
+         root.setTop(kenoMenu.getMenuBar());
+
+	     Scene scene = new Scene(root, 700, 700);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		
