2285bcd64961ccd9a48191666ec6872acef4b234
Jennifer Khamdaraphone <jkhamdaraphone@gmail.com>
2025-10-26 03:15:16 -0500

    rules of game update

--- JavaFX_MavenTemplate_VS1/src/main/java/Odds.java
+++ JavaFX_MavenTemplate_VS1/src/main/java/Odds.java
@@ -2,9 +2,9 @@ public class Odds {
     public static String getText() {
         return "Each draw is completely random.\n" +
                 "The odds depend on how many spots you play.\n" +
-                "1 Spot: 1 in 4 chance of a hit.\n" +
-                "4 Spots: 1 in 3 chance of any win.\n" +
-                "8 Spots: 1 in 9 chance of any win.\n" +
-                "10 Spots: 1 in 15 chance of any win.";
+                "1 Spot: 1 in 4.00.\n" +
+                "4 Spots: 1 in 3.86. \n" +
+                "8 Spots: 1 in 9.77.\n" +
+                "10 Spots: 1 in 9.05.";
     }
 }
