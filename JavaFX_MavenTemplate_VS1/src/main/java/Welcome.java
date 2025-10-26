04814509f8d1e8a3208bd8607881894ee8620007
Jennifer Khamdaraphone <jkhamdaraphone@gmail.com>
2025-10-26 00:11:17 -0500

    added new ui stuff

--- JavaFX_MavenTemplate_VS1/src/main/java/Welcome.java
+++ JavaFX_MavenTemplate_VS1/src/main/java/Welcome.java
@@ -41,7 +41,7 @@ public class Welcome {
 
     public StackPane createWelcomeScreen() {
         StackPane stack = new StackPane();
-        stack.setStyle("-fx-background-color: linear-gradient(to bottom right, #4B0082, #8A2BE2, #FF69B4);");
+        stack.setStyle("-fx-background-color: linear-gradient(to right, #e76366, #8355eb);");
         stack.getChildren().addAll(kenoImage, startButton); // add button here
         StackPane.setAlignment(startButton, Pos.BOTTOM_CENTER);
         startButton.setTranslateY(-150); // move it up a bit from bottom
